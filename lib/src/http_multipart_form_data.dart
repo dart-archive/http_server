c562cf23a39c3bfa484aa906c0667a1cfa4de837
Nate Bosch <nbosch@google.com>
2020-02-27 13:35:32 -0800

    Merge Impl class for HttpMultiparFormData
    Closes #68
    
    Inline the implementation from http_mltipart_form_data_impl.dart
    
    There is a small user visible change in that code which `extends
    HttpMultipartFormData` will become a static error since you can't extend
    a class with only private constructors. Since the previous version had
    no implementation the same use case can be solved with `implements`
    instead of `extends`, and there is no indication any code exists in the
    wild that was using `extends`.
    
    This intentionally avoids other implementation changes like making more
    fields final.

--- lib/src/http_multipart_form_data.dart
+++ lib/src/http_multipart_form_data.dart
@@ -8,8 +8,6 @@ import 'dart:io';
 
 import 'package:mime/mime.dart';
 
-import 'http_multipart_form_data_impl.dart';
-
 /// The data in a `multipart/form-data` part.
 ///
 /// ## Example
@@ -28,37 +26,36 @@ import 'http_multipart_form_data_impl.dart';
 ///
 /// [HttpMultipartFormData] is a Stream, serving either bytes or decoded
 /// Strings. Use [isText] or [isBinary] to see what type of data is provided.
-abstract class HttpMultipartFormData implements Stream {
+class HttpMultipartFormData extends Stream {
   /// The parsed `Content-Type` header value.
   ///
-  /// Returns `null` if not present.
-  ContentType get contentType;
+  /// `null` if not present.
+  final ContentType contentType;
 
   /// The parsed `Content-Disposition` header value.
   ///
   /// This field is always present. Use this to extract e.g. name (form field
   /// name) and filename (client provided name of uploaded file) parameters.
-  HeaderValue get contentDisposition;
+  final HeaderValue contentDisposition;
 
   /// The parsed `Content-Transfer-Encoding` header value.
   ///
   /// This field is used to determine how to decode the data. Returns `null`
   /// if not present.
-  HeaderValue get contentTransferEncoding;
+  final HeaderValue contentTransferEncoding;
+
+  var _isText = false;
 
   /// Whether the data is decoded as [String].
-  bool get isText;
+  bool get isText => _isText;
 
   /// Whether the data is raw bytes.
-  bool get isBinary;
+  bool get isBinary => !_isText;
 
-  /// Returns the value for the header named [name].
+  /// The values which indicate that no incoding was performed.
   ///
-  /// If there is no header with the provided name, `null` will be returned.
-  ///
-  /// Use this method to index other headers available in the original
-  /// [MimeMultipart].
-  String value(String name);
+  /// https://www.w3.org/Protocols/rfc1341/5_Content-Transfer-Encoding.html
+  static const _transparentEncodings = ['7bit', '8bit', 'binary'];
 
   /// Parse a [MimeMultipart] and return a [HttpMultipartFormData].
   ///
@@ -71,6 +68,85 @@ abstract class HttpMultipartFormData implements Stream {
   /// [HTML5 spec](http://dev.w3.org/html5/spec-preview/
   /// constraints.html#multipart-form-data).
   static HttpMultipartFormData parse(MimeMultipart multipart,
-          {Encoding defaultEncoding = utf8}) =>
-      HttpMultipartFormDataImpl.parse(multipart, defaultEncoding);
+      {Encoding defaultEncoding = utf8}) {
+    ContentType type;
+    HeaderValue encoding;
+    HeaderValue disposition;
+    var remaining = <String, String>{};
+    for (var key in multipart.headers.keys) {
+      switch (key) {
+        case 'content-type':
+          type = ContentType.parse(multipart.headers[key]);
+          break;
+
+        case 'content-transfer-encoding':
+          encoding = HeaderValue.parse(multipart.headers[key]);
+          break;
+
+        case 'content-disposition':
+          disposition = HeaderValue.parse(multipart.headers[key],
+              preserveBackslash: true);
+          break;
+
+        default:
+          remaining[key] = multipart.headers[key];
+          break;
+      }
+    }
+    if (disposition == null) {
+      throw const HttpException(
+          "Mime Multipart doesn't contain a Content-Disposition header value");
+    }
+    return HttpMultipartFormData._(
+        type, disposition, encoding, multipart, defaultEncoding);
+  }
+
+  final MimeMultipart _mimeMultipart;
+
+  Stream _stream;
+
+  HttpMultipartFormData._(
+      this.contentType,
+      this.contentDisposition,
+      this.contentTransferEncoding,
+      this._mimeMultipart,
+      Encoding defaultEncoding) {
+    _stream = _mimeMultipart;
+    if (contentTransferEncoding != null &&
+        !_transparentEncodings
+            .contains(contentTransferEncoding.value.toLowerCase())) {
+      // TODO(ajohnsen): Support BASE64, etc.
+      throw HttpException('Unsupported contentTransferEncoding: '
+          '${contentTransferEncoding.value}');
+    }
+
+    if (contentType == null ||
+        contentType.primaryType == 'text' ||
+        contentType.mimeType == 'application/json') {
+      _isText = true;
+      Encoding encoding;
+      if (contentType != null && contentType.charset != null) {
+        encoding = Encoding.getByName(contentType.charset);
+      }
+      encoding ??= defaultEncoding;
+      _stream = _stream.transform(encoding.decoder);
+    }
+  }
+
+  @override
+  StreamSubscription listen(void Function(dynamic) onData,
+      {void Function() onDone, Function onError, bool cancelOnError}) {
+    return _stream.listen(onData,
+        onDone: onDone, onError: onError, cancelOnError: cancelOnError);
+  }
+
+  /// Returns the value for the header named [name].
+  ///
+  /// If there is no header with the provided name, `null` will be returned.
+  ///
+  /// Use this method to index other headers available in the original
+  /// [MimeMultipart].
+  String value(String name) {
+    return _mimeMultipart.headers[name];
+  }
 }
